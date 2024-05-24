0e50136c631c71abfee50ce4ceb40fc5f31be3de
Jang <37791088+dngurqnfrk@users.noreply.github.com>
2024-05-25 00:58:44 +0900

    Start!

--- A2_G7_t2/src/Main.java
+++ A2_G7_t2/src/Main.java
@@ -1,12 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 // A2_G7_t2 : DB scan 알고리즘
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String filePath = args[0];
+
+        if(args.length > 3) {
+            System.out.println("Invalid arguments");
+            return;
+        }
+
+        int minPts;
+        double eps;
+
+        if(args.length == 2) {
+            try{
+                minPts = Integer.parseInt(args[1]);
+                // appropriate eps value
+            } catch (NumberFormatException e) {
+                System.out.println("argument is double");
+                eps  = Double.parseDouble(args[1]);
+                // appropriate minPts value
+            }
+        } else {
+            minPts = Integer.parseInt(args[1]);
+            eps = Double.parseDouble(args[2]);
+        }
+
+        // read all points
+        // 모든 포인트 분류
+        //
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
